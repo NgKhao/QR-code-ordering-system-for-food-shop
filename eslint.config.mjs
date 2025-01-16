3e57d1000d140f32c3cf6ae91b1c6bace72f21cb
NgKhao <bsbnk141@gmail.com>
2025-01-16 22:23:20 +0700

    build UI and logic login

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  ...pluginQuery.configs["flat/recommended"],
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Tắt quy tắc no-explicit-any
+    },
+  },
 ];
 
 export default eslintConfig;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
+import { UseFormSetError } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
+import { toast } from "@/hooks/use-toast";
+import { EntityError } from "./http";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -10,4 +13,40 @@ export function cn(...inputs: ClassValue[]) {
  */
 export const normalizePath = (path: string) => {
   return path.startsWith("/") ? path.slice(1) : path;
+};
+
+export const handleErrorApi = ({
+  error,
+  setError,
+  duration,
+}: {
+  error: any;
+  setError?: UseFormSetError<any>;
+  duration?: number;
+}) => {
+  if (error instanceof EntityError && setError) {
+    error.payload.errors.forEach((item) => {
+      setError(item.field, {
+        type: "server",
+        message: item.message,
+      });
+    });
+  } else {
+    toast({
+      title: "Lỗi",
+      description: error?.payload?.message ?? "Lỗi không xác định",
+      variant: "destructive",
+      duration: duration ?? 5000,
+    });
+  }
+};
+
+const isBrowser = typeof window != "undefined";
+
+export const getAccessTokenFromLocalStorage = () => {
+  return isBrowser ? localStorage.getItem("accessToken") : null;
+};
+
+export const getRefreshTokenToLocalStorage = () => {
+  return isBrowser ? localStorage.getItem("refreshToken") : null;
 };
