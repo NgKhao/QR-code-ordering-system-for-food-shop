95c9d235155e2e3792c5ca172b83f8ccb5ff8f8a
NgKhao <bsbnk141@gmail.com>
2025-02-06 16:13:33 +0700

    update personal profile

--- src/app/manage/setting/update-profile-form.tsx
+++ src/app/manage/setting/update-profile-form.tsx
@@ -13,17 +13,22 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useEffect, useRef, useState } from "react";
-import { useAccountProfile } from "@/queries/useAccount";
+import { useAccountMe, useUpdateMeMutation } from "@/queries/useAccount";
+import { useUploadMediaMutation } from "@/queries/useMedia";
+import { toast } from "@/hooks/use-toast";
+import { handleErrorApi } from "@/lib/utils";
 
 export default function UpdateProfileForm() {
   const [file, setFile] = useState<File | null>(null);
   const avatarInputRef = useRef<HTMLInputElement>(null);
-  const { data } = useAccountProfile();
+  const { data, refetch } = useAccountMe();
+  const updateMeMutation = useUpdateMeMutation();
+  const upLoadMediaMutation = useUploadMediaMutation();
   const form = useForm<UpdateMeBodyType>({
     resolver: zodResolver(UpdateMeBody),
     defaultValues: {
       name: "",
-      avatar: "",
+      avatar: undefined,
     },
   });
 
@@ -33,17 +38,60 @@ export default function UpdateProfileForm() {
   useEffect(() => {
     if (data) {
       const { name, avatar } = data.payload.data;
-      form.reset({ name, avatar: avatar ?? "" });
+      form.reset({ name, avatar: avatar ?? undefined });
     }
   }, [form, data]);
 
   const previewAvatar = file ? URL.createObjectURL(file) : avatar;
+
+  const reset = () => {
+    form.reset();
+    setFile(null);
+  };
+
+  const onSubmit = async (values: UpdateMeBodyType) => {
+    // Kiểm tra nếu mutation đang trong trạng thái pending, không làm gì cả
+    if (updateMeMutation.isPending) return;
+    try {
+      let body = values;
+
+      // Nếu có file (ảnh đại diện mới), thực hiện upload file
+      if (file) {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        // Gọi mutation để upload file và chờ kết quả
+        const uploadImageResult = await upLoadMediaMutation.mutateAsync(
+          formData
+        );
+        const imageUrl = uploadImageResult.payload.data;
+
+        // Cập nhật body với URL của ảnh đại diện mới
+        body = {
+          ...values,
+          avatar: imageUrl,
+        };
+      }
+      const result = await updateMeMutation.mutateAsync(body);
+      toast({
+        description: result.payload.message,
+      });
+      refetch();
+    } catch (error) {
+      handleErrorApi({
+        error,
+        setError: form.setError,
+      });
+    }
+  };
 
   return (
     <Form {...form}>
       <form
         noValidate
         className="grid auto-rows-max items-start gap-4 md:gap-8"
+        onReset={reset}
+        onSubmit={form.handleSubmit(onSubmit)}
       >
         <Card x-chunk="dashboard-07-chunk-0">
           <CardHeader>
@@ -72,6 +120,9 @@ export default function UpdateProfileForm() {
                           const file = e.target.files?.[0];
                           if (file) {
                             setFile(file);
+                            field.onChange(
+                              "http://localhost:3000/" + field.name
+                            );
                           }
                         }}
                       />
