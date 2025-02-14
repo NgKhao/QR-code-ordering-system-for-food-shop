4ec1016a12fa32de224f7264aa219b1789769168
NgKhao <bsbnk141@gmail.com>
2025-02-14 23:34:00 +0700

    create UI template dishes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off", // Tắt quy tắc no-explicit-any
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
 ];
