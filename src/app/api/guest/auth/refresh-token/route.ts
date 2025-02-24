c90052a2086a6d2ef0f50d468daec517a7128319
NgKhao <bsbnk141@gmail.com>
2025-02-24 11:23:03 +0700

    declare API for auth guest

---
+++ src/app/api/guest/auth/refresh-token/route.ts
@@ -0,0 +1,57 @@
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import guestApiRequest from "@/apiRequests/guest";
+
+export async function POST() {
+  const cookieStore = await cookies();
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+  if (!refreshToken) {
+    return Response.json(
+      {
+        message: "Không tìm thấy refreshToken",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  try {
+    const { payload } = await guestApiRequest.sRefreshToken({
+      refreshToken,
+    });
+
+    const decodeAccessToken = jwt.decode(payload.data.accessToken) as {
+      exp: number;
+    };
+    const decodeRefreshToken = jwt.decode(payload.data.refreshToken) as {
+      exp: number;
+    };
+
+    cookieStore.set("accessToken", payload.data.accessToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodeAccessToken.exp * 1000,
+    });
+
+    cookieStore.set("refreshToken", payload.data.refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodeRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
+  } catch (error: any) {
+    console.log(error);
+    return Response.json(
+      {
+        message: error.message ?? "co loi xay ra",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+}
