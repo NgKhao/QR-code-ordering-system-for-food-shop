bbdeb9f13eefd7ac6ee4aac5429b1dee25fe9c3f
NgKhao <bsbnk141@gmail.com>
2025-02-28 17:57:29 +0700

    create UI manage orders

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off", // Tắt quy tắc no-explicit-any
       "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
