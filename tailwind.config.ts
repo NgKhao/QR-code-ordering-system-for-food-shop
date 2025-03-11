425463fecb85fb79ba9b5d6e0753216828915255
NgKhao <bsbnk141@gmail.com>
2025-03-11 17:27:35 +0700

    fix: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss-animate";
 
 export default {
     darkMode: ["class"],
@@ -58,5 +59,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [plugin],
 } satisfies Config;
